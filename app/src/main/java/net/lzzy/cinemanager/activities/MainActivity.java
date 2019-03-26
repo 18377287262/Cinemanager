cf5bbe08c78ea9b7172e7019c1ef7db0b246fd22
Administrator <2714621751@qqcom>
2019-03-26 15:37:04 +0800

    自定义标题栏 （3月26日）3:36

--- app/src/main/java/net/lzzy/cinemanager/activities/MainActivity.java
+++ app/src/main/java/net/lzzy/cinemanager/activities/MainActivity.java
@@ -3,17 +3,52 @@ package net.lzzy.cinemanager.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.SearchView;
+import android.widget.TextView;
 
 import net.lzzy.cinemanager.R;
 
 /**
  * @author Administrator
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private View layoutMenu;
+    private SearchView search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /** 去掉标题栏 **/
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+
+        serTitleMenu();
     }
+
+    private void serTitleMenu() {
+        layoutMenu=findViewById(R.id.bar_title_layout_menu);
+        layoutMenu.setVisibility(View.GONE);
+        findViewById(R.id.bar_title_img_menu).setOnClickListener(v -> {
+            int visible=layoutMenu.getVisibility()==View.VISIBLE?View.GONE:View.VISIBLE;
+            layoutMenu.setVisibility(visible);
+        });
+        TextView tvTitle=findViewById(R.id.bar_title_tv_title);
+        tvTitle.setText("我的订单");
+        search=findViewById(R.id.bar_title_search);
+        findViewById(R.id.bar_title_tv_add_cinema).setOnClickListener(this);
+        findViewById(R.id.bar_title_tv_view_cinema).setOnClickListener(this);
+        findViewById(R.id.bar_title_tv_add_order).setOnClickListener(this);
+        findViewById(R.id.bar_title_tv_view_order).setOnClickListener(this);
+        findViewById(R.id.bar_title_tv_exit).setOnClickListener(v -> System.exit(0));
+
+    }
+    @Override
+    public void onClick(View v) {
+    }
+
 }
+
